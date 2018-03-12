fe3d90aaa398b71110be51dafc90efd69dabe9c5
jianxia <jianxia@xfz.cn>
2018-03-12 10:53:24 +0800

    modify axiosconfig

--- front/src/assets/js/axiosConfig.js
+++ front/src/assets/js/axiosConfig.js
@@ -10,7 +10,7 @@ axios.defaults.baseURL = '/api';
 //添加请求拦截器
 axios.interceptors.request.use(config => {
     //在发送请求之前做某事
-    return Promise.resolve(config);
+    return config;
 }, error => {
     //请求错误时做些事
     return Promise.reject(error);
@@ -20,7 +20,7 @@ axios.interceptors.request.use(config => {
 //添加响应拦截器
 axios.interceptors.response.use(response => {
     // 对响应数据做些事
-    return Promise.resolve(response);
+    return response;
 }, error => {
     // 请求错误时做些事
     return Promise.reject(error);
