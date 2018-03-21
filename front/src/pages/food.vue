c8d967481fa094de68da4479d89bb10980b20618
jianxia <jianxia@xfz.cn>
2018-03-21 14:17:53 +0800

    update foods

--- front/src/pages/food.vue
+++ front/src/pages/food.vue
@@ -2,8 +2,8 @@
     <div class="food">
         <div class="content">
             <div class="header clearfix">
-                <h5>美食列表</h5>
-                <router-link to="/addfood" class="router-link add-food">上传自己的菜肴</router-link>
+                <p class="title">美食列表</p>
+                <p class="add-food" @click="gotoAdd">上传自己的菜肴</p>
             </div>
             <ul class="ul-content">
                 <li v-for="eitem in foodList">
@@ -33,56 +33,70 @@ export default {
                     this.foodList = res.data.data
                 }
             })
+        },
+        gotoAdd () {
+            this.$router.push('/addfood')
         }
     }
 }
 </script>
 <style lang="scss">
-    .food {
-        box-sizing: border-box;
-        padding: 20px 0;
-        width: 940px;
-        margin: auto;
-
-        .content {
-            margin-top: 35px;
-            background: #fff;
-            padding: 20px;
-            overflow: hidden;
-            .header {
-                h5 {
-                    color: #12937c;
-                    float: left;
-                }
-                .add-food {
-                    padding: 10px 20px;
-                    border: 1px solid #35495e;
-                    float: right;
-                }
+.food {
+    box-sizing: border-box;
+    padding: 20px 0;
+    width: 940px;
+    margin: auto;
+    .content {
+        margin-top: 35px;
+        background: #fff;
+        padding: 20px;
+        overflow: hidden;
+        .header {
+            width: 100%;
+            height: 80px;
+            p.title {
+                display: inline-block;
+                color: #12937c;
+                font-size: 18px;
+                line-height: 80px;
             }
-            .ul-content {
-                margin-top: 30px;
-                li {
-                    cursor: pointer;
-                    box-sizing: border-box;
-                    padding: 20px;
-                    background: #daeae8;
-                    width: 200px;
-                    height: 200px;
-                    float: left;
-                    text-align: center;
-                    margin-left: 20px;
-                    margin-bottom: 20px;
-                    img {
-                        width: 160px;
-                        height: 140px;
-                    }
-                    p {
-                        line-height: 40px;
-                        color: #b1a058;
-                    }
+            p.add-food {
+                display: inline-block;
+                margin-top: 20px;
+                cursor: pointer;
+                background: #ff9090;
+                color: #fff;
+                text-align: center;
+                width: 180px;
+                height: 40px;
+                line-height: 40px;
+                border: 1px solid #ff9090;
+                float: right;
+            }
+        }
+        .ul-content {
+            margin-top: 30px;
+            li {
+                cursor: pointer;
+                box-sizing: border-box;
+                padding: 20px;
+                background: #daeae8;
+                width: 200px;
+                height: 200px;
+                float: left;
+                text-align: center;
+                margin-left: 20px;
+                margin-bottom: 20px;
+                img {
+                    width: 160px;
+                    height: 140px;
+                }
+                p {
+                    line-height: 40px;
+                    color: #b1a058;
                 }
             }
         }
     }
+}
 </style>
